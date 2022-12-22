1da9421e1b4ae7f88e4a5d5343a6b308ca119c52
a.nezgovorova <a.nezgovorova@swooapp.com>
2022-12-22 18:54:14 +0300

    First commit

---
+++ common/src/commonMain/kotlin/com/nstnz/collector/common/feature/main/presentation/MainScreen.kt
@@ -0,0 +1,13 @@
+package com.nstnz.collector.common.feature.main.presentation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+internal fun MainScreen() {
+    Box(Modifier.fillMaxSize().background(Color.Cyan))
+}
